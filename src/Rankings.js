22858b23825173a67c10d7942c69adbdd5e62e69
Eddie Reinhardt <rothreinhardts@gmail.com>
2023-12-06 12:47:49 -0600

    Delete src/Rankings.js

--- src/Rankings.js
+++
@@ -1,9 +0,0 @@
-import play_game from "./Simulation";
-
-
-const teams = ["MIN","DET","GNB","CHI","DAL","PHI","WAS","NYG","ATL","NOR","TAM","CAR","LAR","ARI","SFO","SEA",
-"BAL","PIT","CIN","CLE","MIA","NWE","NYJ","BUF","IND","HOU","TEN","JAX","KAN","DEN","LVR","LAC"]
-
-function power_rankings(QBvalues, RBvalues, Pdefvalues, Rdefvalues, Offvalues){
-    
-}
